6347a4efe379707a95c2f31f1811a08015220af3
Kyle Moser <moser.kyle@gmail.com>
2022-03-13 15:53:50 -0400

    Better data for CSV rows

--- csv/helpers.go
+++ csv/helpers.go
@@ -1,8 +1,15 @@
 package csv
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-func FormatDatetime(dt time.Time) string {
-	formatter := "mm/dd/yyyy hh:MM:s"
-	return dt.Format(formatter)
+func FormatDatetime(t time.Time) string {
+	//mm/dd/yyyy hh:MM:s
+	result := fmt.Sprintf("%02d/%02d/%d %02d:%02d:%02d",
+		t.Month(), t.Day(), t.Year(),
+		t.Hour(), t.Minute(), t.Second())
+
+	return result
 }
--- db/search.go
+++ db/search.go
@@ -7,7 +7,8 @@ func GetTaxableEvents(address string, db *gorm.DB) ([]TaxableEvent, error) {
 	var taxableEvents []TaxableEvent
 
 	result := db.Joins("JOIN addresses ON addresses.id = taxable_events.sender_address_id OR addresses.id = taxable_events.receiver_address_id").
-		Where("addresses.address = ?", address).Find(&taxableEvents)
+		Where("addresses.address = ?", address).Preload("Message").Preload("Message.Tx").
+		Preload("SenderAddress").Preload("ReceiverAddress").Find(&taxableEvents)
 
 	return taxableEvents, result.Error
 }
