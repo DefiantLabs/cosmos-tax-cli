128907f7c88087426654d047c8a93bddd47aa291
pharr117 <pjharral@gmail.com>
2022-10-09 18:26:01 -0400

    Build up API for CSV request: * Allow format, address, chain and date range in body * Basic param validation * CORs middleware, allow all. Allow OPTIONS request pre-POST as required by Javascript fetch

--- client/client.go
+++ client/client.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/DefiantLabs/cosmos-tax-cli/config"
 	configHelpers "github.com/DefiantLabs/cosmos-tax-cli/config"
-
 	"github.com/DefiantLabs/cosmos-tax-cli/csv"
+
 	dbTypes "github.com/DefiantLabs/cosmos-tax-cli/db"
 
 	"github.com/gin-gonic/gin"
@@ -15,13 +16,14 @@ import (
 )
 
 var DB *gorm.DB
+var Config *config.Config
 
-func setup() (*gorm.DB, error) {
+func setup() (*gorm.DB, *config.Config, error) {
 
 	argConfig, err := configHelpers.ParseArgs(os.Stderr, os.Args[1:])
 
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	var location string
@@ -35,7 +37,7 @@ func setup() (*gorm.DB, error) {
 
 	if err != nil {
 		fmt.Println("Error opening configuration file", err)
-		return nil, err
+		return nil, nil, err
 	}
 
 	config := configHelpers.MergeConfigs(fileConfig, argConfig)
@@ -44,20 +46,21 @@ func setup() (*gorm.DB, error) {
 		config.Database.User, config.Database.Password, logLevel)
 	if err != nil {
 		fmt.Println("Could not establish connection to the database", err)
-		return nil, err
+		return nil, nil, err
 	}
 
 	dbTypes.CacheDenoms(db)
 
-	return db, nil
+	return db, &config, nil
 
 }
 
 func main() {
 
-	db, err := setup()
+	db, config, err := setup()
 
 	DB = db
+	Config = config
 
 	if err != nil {
 		fmt.Println("Error setting up")
@@ -66,28 +69,80 @@ func main() {
 	}
 
 	r := gin.Default()
-	r.GET("/events.csv", GetTaxableEventsCSV)
+	r.Use(CORSMiddleware())
+
+	r.POST("/events.csv", GetTaxableEventsCSV)
 	r.Run(":8080")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//Probably want to lock CORs down later, will need to know the hostname of the UI server
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+type TaxableEventsCSVRequest struct {
+	Chain     string  `json:"chain"`
+	Address   string  `json:"address"`
+	StartDate *string `json:"startDate"` //can be null
+	EndDate   *string `json:"endDate"`   //can be null
+	Format    string  `json:"format"`
 }
 
 func GetTaxableEventsCSV(c *gin.Context) {
 
-	params := c.Request.URL.Query()
-	address := params["address"][0]
-
-	accountRows, err := csv.ParseForAddress(address, DB)
-
+	var requestBody TaxableEventsCSVRequest
+	err := c.BindJSON(&requestBody)
 	if err != nil {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.JSON(404, gin.H{
-			"message": "No events for the given address",
-		})
-		c.AbortWithError(404, errors.New("No events for the given address"))
+		c.AbortWithError(500, errors.New("Error processing request body"))
 		return
 	}
 
-	buffer := csv.ToCsv(accountRows)
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Data(200, "text/csv", buffer.Bytes())
+	//We expect ISO 8601 dates in UTC
+	var startDate string
+	if requestBody.StartDate != nil {
+		startDate = *requestBody.StartDate
+	}
 
+	var endDate string
+	if requestBody.EndDate != nil {
+		endDate = *requestBody.EndDate
+	}
+
+	if requestBody.Address == "" {
+		c.JSON(422, gin.H{"message": "Address is required"})
+		return
+	}
+	fmt.Printf("Start: %s End: %s\n", startDate, endDate)
+
+	if requestBody.Format == "" {
+		c.JSON(422, gin.H{"message": "Format is required"})
+		return
+	}
+
+	accountRows, headers, err := csv.ParseForAddress(requestBody.Address, DB, requestBody.Format, *Config)
+
+	if err != nil {
+		c.AbortWithError(500, errors.New("Error getting rows for address"))
+		return
+	}
+
+	if len(accountRows) == 0 {
+		c.JSON(404, gin.H{"message": "No transactions for given address"})
+		return
+	}
+
+	buffer := csv.ToCsv(accountRows, headers)
+	c.Data(200, "text/csv", buffer.Bytes())
 }
