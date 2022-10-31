2b5f3089a11673e41643890202455bd7d6a288e5
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2022-10-31 12:38:54 -0400

    add tests to the CI

---
+++ csv/parser_test.go
@@ -0,0 +1,11 @@
+package csv
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTest(t *testing.T) {
+	testTrue := true
+	assert.True(t, testTrue, "this should be true")
+}
