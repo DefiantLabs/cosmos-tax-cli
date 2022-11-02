e5728b08e6993b93529ad46e9374aeca05507638
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2022-11-02 12:02:12 -0400

    spread out gamms while indexing

---
+++ osmosis/modules/gamm/gamm_test.go
@@ -0,0 +1,16 @@
+package gamm
+
+import (
+	"github.com/stretchr/testify/assert"
+	"math/big"
+	"testing"
+)
+
+// TODO: Write test to assert that osmosis rewards (aka taxable events) are tagged as deposits and classified as 'liquidity_pool'
+
+func TestGammCalc(t *testing.T) {
+	bignum := big.NewInt(int64(100))
+	nthGamms, remainderGamms := calcNthGams(bignum, 3)
+	assert.Equalf(t, nthGamms, big.NewInt(int64(33)), "1/3 of 100 rounds to 33")
+	assert.Equalf(t, remainderGamms, big.NewInt(int64(34)), "the 3rd 3rd will get 34")
+}
