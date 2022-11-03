a6c6fb445cfa068f15b3c23c3b50845e444761c8
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2022-11-03 11:19:32 -0400

    update tests

--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/DefiantLabs/cosmos-tax-cli-private/core"
 	"github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"os"
 	"strings"
 	"testing"
@@ -27,17 +28,14 @@ func getHomePath(t *testing.T) string {
 	return fmt.Sprintf("%v/.lens", homeDir)
 }
 
-// setup does pre-run setup configurations.
-//   - Loads the application config from config.tml, cli args and parses/merges
-//   - Connects to the database and returns the db object
-//   - Returns various values used throughout the application
-//
 //nolint:unused
-func setupRPC() (*config.Config, *gocron.Scheduler, error) {
-	argConfig, err := config.ParseArgs(os.Stderr, os.Args[1:])
+var testConfig *config.Config
 
+//nolint:unused
+func setupConfig(t *testing.T) {
+	argConfig, err := config.ParseArgs(os.Stderr, os.Args[1:])
 	if err != nil {
-		return nil, nil, err
+		assert.Nil(t, err)
 	}
 
 	var location string
@@ -48,10 +46,8 @@ func setupRPC() (*config.Config, *gocron.Scheduler, error) {
 	}
 
 	fileConfig, err := config.GetConfig(location)
-
 	if err != nil {
-		fmt.Println("Error opening configuration file", err)
-		return nil, nil, err
+		assert.Nil(t, err)
 	}
 
 	cfg := config.MergeConfigs(fileConfig, argConfig)
@@ -60,18 +56,30 @@ func setupRPC() (*config.Config, *gocron.Scheduler, error) {
 	if cfg.Base.StartBlock == 0 {
 		cfg.Base.StartBlock = 1
 	}
+	testConfig = &cfg
+}
 
+// setup does pre-run setup configurations.
+//   - Loads the application config from config.tml, cli args and parses/merges
+//   - Connects to the database and returns the db object
+//   - Returns various values used throughout the application
+//
+//nolint:unused
+func setupRPC(t *testing.T) *gocron.Scheduler {
+	if testConfig == nil {
+		setupConfig(t)
+	}
 	//TODO: create config values for the prefixes here
 	//Could potentially check Node info at startup and pass in ourselves?
-	core.SetupAddressRegex("juno(valoper)?1[a-z0-9]{38}")
-	core.SetupAddressPrefix("juno")
+	core.SetupAddressRegex(testConfig.Base.AddressRegex)
+	core.SetupAddressPrefix(testConfig.Base.AddressPrefix)
 
 	scheduler := gocron.NewScheduler(time.UTC)
-	return &cfg, scheduler, nil
+	return scheduler
 }
 
 func TestRPC(t *testing.T) {
-	block := 2
+	block := 2620000
 	err := lensQueryBank(t, int64(block))
 	if err != nil {
 		assert.Nil(t, err, "should not error writing to CSV")
@@ -84,11 +92,7 @@ func TestRPC(t *testing.T) {
 }
 
 func TestDecodeIBCTypes(t *testing.T) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		assert.Nil(t, err)
-	}
-	cl := GetOsmosisTestClient(fmt.Sprintf("%v/.lens", homeDir))
+	cl := GetOsmosisTestClient(t)
 	resp, err := GetTxsByBlockHeight(cl, 2620000)
 	assert.Empty(t, err)
 	hasIbcType := false
@@ -114,7 +118,7 @@ func TestDecodeIBCTypes(t *testing.T) {
 	assert.True(t, hasIbcType)
 }
 
-func GetTestClient(t *testing.T) *lensClient.ChainClient {
+func GetJunoTestClient(t *testing.T) *lensClient.ChainClient {
 	homepath := getHomePath(t)
 	//IMPORTANT: the actual keyring-test will be searched for at the path {homepath}/keys/{ChainID}/keyring-test.
 	//You can use lens default settings to generate that directory appropriately then move it to the desired path.
@@ -126,12 +130,14 @@ func GetTestClient(t *testing.T) *lensClient.ChainClient {
 	return cl
 }
 
-func GetOsmosisTestClient(homepath string) *lensClient.ChainClient {
+func GetOsmosisTestClient(t *testing.T) *lensClient.ChainClient {
+	homepath := getHomePath(t)
 	//IMPORTANT: the actual keyring-test will be searched for at the path {homepath}/keys/{ChainID}/keyring-test.
 	//You can use lens default settings to generate that directory appropriately then move it to the desired path.
 	//For example, 'lens keys restore default' will restore the key to the default keyring (e.g. /home/kyle/.lens/...)
 	//and you can move all of the necessary keys to whatever homepath you want to use. Or you can use --home flag.
-	cl, _ := lensClient.NewChainClient(GetOsmosisConfig(homepath, true), homepath, nil, nil)
+	cl, err := lensClient.NewChainClient(GetOsmosisConfig(homepath, true), homepath, nil, nil)
+	assert.Nil(t, err)
 	config.RegisterAdditionalTypes(cl)
 	return cl
 }
@@ -156,6 +162,7 @@ func GetJunoConfig(keyHome string, debug bool) *lensClient.ChainClientConfig {
 }
 
 func GetOsmosisConfig(keyHome string, debug bool) *lensClient.ChainClientConfig {
+	log.Println(keyHome)
 	return &lensClient.ChainClientConfig{
 		Key:            "default",
 		ChainID:        "osmosis-1",
@@ -175,11 +182,11 @@ func GetOsmosisConfig(keyHome string, debug bool) *lensClient.ChainClientConfig 
 }
 
 func lensQueryBank(t *testing.T, height int64) error {
-	cl := GetTestClient(t)
+	cl := GetOsmosisTestClient(t)
 	keyNameOrAddress := cl.Config.Key
-
 	address, err := cl.AccountFromKeyOrAddress(keyNameOrAddress)
 	if err != nil {
+		log.Println("Error getting account from key or address: ", keyNameOrAddress)
 		return err
 	}
 	encodedAddr := cl.MustEncodeAccAddr(address)
@@ -191,7 +198,7 @@ func lensQueryBank(t *testing.T, height int64) error {
 }
 
 func rpcQueryTx(t *testing.T, height int64) error {
-	cl := GetTestClient(t)
+	cl := GetOsmosisTestClient(t)
 	//requestEndpoint := fmt.Sprintf(rest.GetEndpoint("txs_by_block_height_endpoint"), height)
 	options := lensQuery.QueryOptions{Height: height}
 	query := lensQuery.Query{Client: cl, Options: &options}
