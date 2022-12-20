c738c318df8c4355a7595b9b35e27808132cf380
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2022-12-20 12:34:28 -0500

    154 index failed osmosis block 4077 (#160)
    * frame out plan to fix issue
    
    * Hacky fix to get pool join info

--- osmosis/modules/gamm/types.go
+++ osmosis/modules/gamm/types.go
@@ -434,7 +434,35 @@ func (sf *WrapperMsgJoinPool) HandleMsg(msgType string, msg sdk.Msg, log *txModu
 	// Address of whoever initiated the join
 	poolJoinedEvent := txModule.GetEventWithType(gammTypes.TypeEvtPoolJoined, log)
 	if poolJoinedEvent == nil {
-		return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
+		// If the message doesn't have the pool_joined event, we can parse the transaction event to extract the
+		// amounts of the tokens they transferred in.
+
+		// find the multi-coin amount that also is not gamms... those must be the coins transferred in
+		var tokensIn string
+		var sender string
+		for i, attr := range transferEvt.Attributes {
+			if attr.Key == "amount" && !strings.Contains(attr.Value, "gamm/pool") && strings.Contains(attr.Value, ",") {
+				tokensIn = attr.Value
+				// If we haven't found the sender yet, it will be the address that sent this non-gamm token
+				if i > 0 && transferEvt.Attributes[i-1].Key == "sender" {
+					sender = transferEvt.Attributes[i-1].Value
+				}
+				break
+			}
+		}
+		// if either of these methods failed to get info, give up and return an error
+		if len(tokensIn) == 0 || sender == "" {
+			return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
+		}
+
+		// if we got what we needed, set the correct values and return successfully
+		sf.Address = sender
+		sf.TokensIn, err = sdk.ParseCoinsNormalized(tokensIn)
+		if err != nil {
+			fmt.Println("Error parsing coins")
+			return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
+		}
+		return nil
 	}
 
 	// Address of whoever initiated the join.
@@ -450,7 +478,6 @@ func (sf *WrapperMsgJoinPool) HandleMsg(msgType string, msg sdk.Msg, log *txModu
 		return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
 	}
 	sf.TokensIn, err = sdk.ParseCoinsNormalized(tokensInString)
-
 	if err != nil {
 		return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
 	}
