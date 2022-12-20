941e4198b14160fe259b34e9fe6456ca6f561189
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2022-12-20 14:30:23 -0500

    replace old logger with new one

--- config/logger.go
+++ config/logger.go
@@ -1,43 +1,65 @@
 package config
 
 import (
-	lg "log"
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/rs/zerolog"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
-var Log *zap.Logger // Global logger
+type Logger struct {
+	ZeroLogger *zerolog.Logger
+}
+
+// Log is exposed on the config as a drop-in replacement for our old logger
+var Log Logger
+
+func (l *Logger) Fatal(msg string, err ...zap.Field) {
+	l.ZeroLogger.Fatal().Msg(fmt.Sprint(msg, err))
+}
+
+func (l *Logger) Error(msg string, err ...zap.Field) {
+	l.ZeroLogger.Error().Msg(fmt.Sprint(msg, err))
+}
+
+func (l *Logger) Debug(msg string, err ...zap.Field) {
+	l.ZeroLogger.Debug().Msg(fmt.Sprint(msg, err))
+}
+
+func (l *Logger) Warn(msg string, err ...zap.Field) {
+	l.ZeroLogger.Warn().Msg(fmt.Sprint(msg, err))
+}
+
+func (l *Logger) Info(msg string, err ...zap.Field) {
+	l.ZeroLogger.Info().Msg(fmt.Sprint(msg, err))
+}
 
 func DoConfigureLogger(logPath string, logLevel string) {
-	// only log to path if set
-	outputPaths := []string{"stdout"}
-	if len(logPath) > 0 {
-		outputPaths = append(outputPaths, logPath)
+	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	Log.ZeroLogger = &logger
+
+	// Set the log level (default to info)
+	switch strings.ToLower(logLevel) {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	case "panic":
+		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
 
-	// Logger
-	cfg := zap.Config{
-		OutputPaths: outputPaths,
-		EncoderConfig: zapcore.EncoderConfig{
-			MessageKey:  "message",
-			LevelKey:    "level",
-			EncodeLevel: zapcore.LowercaseLevelEncoder,
-			EncodeTime:  zapcore.ISO8601TimeEncoder,
-			TimeKey:     "timestamp",
-		},
-		Encoding: "json",
-		Level:    zap.NewAtomicLevel(),
-	}
+	// Cmds like this can be called to modify the logger time format
+	// zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	al, err := zap.ParseAtomicLevel(logLevel)
-	if err != nil {
-		lg.Fatalf("logger setup failure. Err: %v", err)
-	}
-
-	cfg.Level = al
-	Log, err = cfg.Build()
-	if err != nil {
-		lg.Fatalf("logger setup failure. Err: %v", err)
-	}
+	// FIXME: figure out how to log to a file.
 }
--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 require (
 	github.com/cosmos/ibc-go/v3 v3.0.0
 	github.com/preichenberger/go-coinbasepro/v2 v2.1.0
+	github.com/rs/zerolog v1.28.0
 )
 
 require (
@@ -106,7 +107,8 @@ require (
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/libp2p/go-buffer-pool v0.0.2 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/mimoo/StrobeGo v0.0.0-20220103164710-9a04d6ca976b // indirect
 	github.com/minio/highwayhash v1.0.2 // indirect
@@ -128,7 +130,6 @@ require (
 	github.com/regen-network/cosmos-proto v0.3.1 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/rs/cors v1.8.2 // indirect
-	github.com/rs/zerolog v1.26.0 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
@@ -148,7 +149,7 @@ require (
 	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
 	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/term v0.0.0-20220411215600-e5f449aeb171 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
