20743bac833f1c0537128945641742b45314b5c3
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2022-12-28 09:24:33 -0500

    add superfluid msg types

---
+++ osmosis/modules/superfluid/types.go
@@ -0,0 +1,8 @@
+package superfluid
+
+const (
+	MsgSuperfluidDelegate        = "/osmosis.superfluid.MsgSuperfluidDelegate"
+	MsgSuperfluidUndelegate      = "/osmosis.superfluid.MsgSuperfluidUndelegate"
+	MsgSuperfluidUnbondLock      = "/osmosis.superfluid.MsgSuperfluidUnbondLock"
+	MsgLockAndSuperfluidDelegate = "/osmosis.superfluid.MsgLockAndSuperfluidDelegate"
+)
