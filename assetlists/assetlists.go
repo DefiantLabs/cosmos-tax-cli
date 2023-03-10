a190a717d4485b73ffc5d75af7bc824e3bb9fbc6
pharr117 <pjharral@gmail.com>
2023-03-10 01:03:29 -0500

    Add asset list types and a method for retrieving them from a URL, use in Koinly to receive DefiantLabs asset list, parse to mapping and replace symbols with koinly IDs

---
+++ assetlists/assetlists.go
@@ -0,0 +1,26 @@
+package assetlists
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func GetAssetList(url string) (AssetList, error) {
+	var assetList AssetList
+	resp, err := http.Get(url)
+	if err != nil {
+		return assetList, err
+	}
+	defer resp.Body.Close()
+
+	resBody, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return assetList, err
+	}
+
+	err = json.Unmarshal(resBody, &assetList)
+
+	return assetList, err
+}
