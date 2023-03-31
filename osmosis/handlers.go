8e7c97b2b1e8960808348bb76df0a49c76435c2a
pharr117 <24580777+pharr117@users.noreply.github.com>
2023-03-31 00:34:03 -0400

    #340 Osmosis v15 support (#365)
    * #340: Add support for Osmosis v15 by bumping Lens fork version. TODO: Add support for new pool manager types MsgSwapExactAmountIn and Out
    
    * Add support for poolmanager SwapExactAmountIn and SwapExactAmountOut message types
    
    * Logger updates
    
    * Precommit go mod tidy

--- osmosis/handlers.go
+++ osmosis/handlers.go
@@ -3,17 +3,20 @@ package osmosis
 import (
 	txTypes "github.com/DefiantLabs/cosmos-tax-cli/cosmos/modules/tx"
 	"github.com/DefiantLabs/cosmos-tax-cli/osmosis/modules/gamm"
+	"github.com/DefiantLabs/cosmos-tax-cli/osmosis/modules/poolmanager"
 )
 
 // Unmarshal JSON to a particular type.
 var MessageTypeHandler = map[string][]func() txTypes.CosmosMessage{
-	gamm.MsgSwapExactAmountIn:       {func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountIn{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountIn2{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountIn3{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountIn4{} }},
-	gamm.MsgSwapExactAmountOut:      {func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountOut{} }},
-	gamm.MsgJoinSwapExternAmountIn:  {func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinSwapExternAmountIn{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinSwapExternAmountIn2{} }},
-	gamm.MsgJoinSwapShareAmountOut:  {func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinSwapShareAmountOut{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinSwapShareAmountOut2{} }},
-	gamm.MsgJoinPool:                {func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinPool{} }},
-	gamm.MsgExitSwapShareAmountIn:   {func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitSwapShareAmountIn{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitSwapShareAmountIn2{} }},
-	gamm.MsgExitSwapExternAmountOut: {func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitSwapExternAmountOut{} }},
-	gamm.MsgExitPool:                {func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitPool{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitPool2{} }},
-	gamm.MsgCreatePool:              {func() txTypes.CosmosMessage { return &gamm.WrapperMsgCreatePool{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgCreatePool2{} }},
+	gamm.MsgSwapExactAmountIn:         {func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountIn{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountIn2{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountIn3{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountIn4{} }},
+	gamm.MsgSwapExactAmountOut:        {func() txTypes.CosmosMessage { return &gamm.WrapperMsgSwapExactAmountOut{} }},
+	gamm.MsgJoinSwapExternAmountIn:    {func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinSwapExternAmountIn{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinSwapExternAmountIn2{} }},
+	gamm.MsgJoinSwapShareAmountOut:    {func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinSwapShareAmountOut{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinSwapShareAmountOut2{} }},
+	gamm.MsgJoinPool:                  {func() txTypes.CosmosMessage { return &gamm.WrapperMsgJoinPool{} }},
+	gamm.MsgExitSwapShareAmountIn:     {func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitSwapShareAmountIn{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitSwapShareAmountIn2{} }},
+	gamm.MsgExitSwapExternAmountOut:   {func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitSwapExternAmountOut{} }},
+	gamm.MsgExitPool:                  {func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitPool{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgExitPool2{} }},
+	gamm.MsgCreatePool:                {func() txTypes.CosmosMessage { return &gamm.WrapperMsgCreatePool{} }, func() txTypes.CosmosMessage { return &gamm.WrapperMsgCreatePool2{} }},
+	poolmanager.MsgSwapExactAmountIn:  {func() txTypes.CosmosMessage { return &poolmanager.WrapperMsgSwapExactAmountIn{} }},
+	poolmanager.MsgSwapExactAmountOut: {func() txTypes.CosmosMessage { return &poolmanager.WrapperMsgSwapExactAmountOut{} }},
 }
---
+++ osmosis/modules/poolmanager/types.go
@@ -0,0 +1,152 @@
+package poolmanager
+
+import (
+	"fmt"
+	"strconv"
+
+	parsingTypes "github.com/DefiantLabs/cosmos-tax-cli/cosmos/modules"
+	txModule "github.com/DefiantLabs/cosmos-tax-cli/cosmos/modules/tx"
+	"github.com/DefiantLabs/cosmos-tax-cli/util"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	poolManagerTypes "github.com/osmosis-labs/osmosis/v15/x/poolmanager/types"
+)
+
+const (
+	MsgSwapExactAmountIn  = "/osmosis.poolmanager.v1beta1.MsgSwapExactAmountIn"
+	MsgSwapExactAmountOut = "/osmosis.poolmanager.v1beta1.MsgSwapExactAmountOut"
+)
+
+type WrapperMsgSwapExactAmountIn struct {
+	txModule.Message
+	OsmosisMsgSwapExactAmountIn *poolManagerTypes.MsgSwapExactAmountIn
+	Address                     string
+	TokenOut                    sdk.Coin
+	TokenIn                     sdk.Coin
+}
+
+type WrapperMsgSwapExactAmountOut struct {
+	txModule.Message
+	OsmosisMsgSwapExactAmountOut *poolManagerTypes.MsgSwapExactAmountOut
+	Address                      string
+	TokenOut                     sdk.Coin
+	TokenIn                      sdk.Coin
+}
+
+func (sf *WrapperMsgSwapExactAmountIn) String() string {
+	var tokenSwappedOut string
+	var tokenSwappedIn string
+	if !sf.TokenOut.IsNil() {
+		tokenSwappedOut = sf.TokenOut.String()
+	}
+	if !sf.TokenIn.IsNil() {
+		tokenSwappedIn = sf.TokenIn.String()
+	}
+
+	return fmt.Sprintf("MsgSwapExactAmountIn (pool-manager): %s swapped in %s and received %s\n",
+		sf.Address, tokenSwappedIn, tokenSwappedOut)
+}
+
+func (sf *WrapperMsgSwapExactAmountOut) String() string {
+	var tokenSwappedOut string
+	var tokenSwappedIn string
+	if !sf.TokenOut.IsNil() {
+		tokenSwappedOut = sf.TokenOut.String()
+	}
+	if !sf.TokenIn.IsNil() {
+		tokenSwappedIn = sf.TokenIn.String()
+	}
+	return fmt.Sprintf("MsgSwapExactAmountOut (pool-manager): %s swapped in %s and received %s\n",
+		sf.Address, tokenSwappedIn, tokenSwappedOut)
+}
+
+func (sf *WrapperMsgSwapExactAmountIn) HandleMsg(msgType string, msg sdk.Msg, log *txModule.LogMessage) error {
+	sf.Type = msgType
+	sf.OsmosisMsgSwapExactAmountIn = msg.(*poolManagerTypes.MsgSwapExactAmountIn)
+	// Confirm that the action listed in the message log matches the Message type
+	validLog := txModule.IsMessageActionEquals(sf.GetType(), log)
+	if !validLog {
+		return util.ReturnInvalidLog(msgType, log)
+	}
+
+	sf.TokenIn = sf.OsmosisMsgSwapExactAmountIn.TokenIn
+	sf.Address = sf.OsmosisMsgSwapExactAmountIn.Sender
+
+	// The attribute in the log message that shows you the tokens swapped
+	tokensSwappedEvt := txModule.GetEventWithType("token_swapped", log)
+	if tokensSwappedEvt == nil {
+		return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
+	}
+
+	// The last route in the hops gives the token out denom and pool ID for the final output
+	lastRoute := sf.OsmosisMsgSwapExactAmountIn.Routes[len(sf.OsmosisMsgSwapExactAmountIn.Routes)-1]
+	lastRouteDenom := lastRoute.TokenOutDenom
+	lastRoutePoolId := lastRoute.PoolId
+
+	tokenOutStr := txModule.GetLastValueForAttribute("tokens_out", tokensSwappedEvt)
+	tokenOutPoolId := txModule.GetLastValueForAttribute("pool_id", tokensSwappedEvt)
+
+	tokenOut, err := sdk.ParseCoinNormalized(tokenOutStr)
+	if err != nil {
+		return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
+	}
+
+	// Sanity check last route swap
+	if tokenOut.Denom != lastRouteDenom || strconv.FormatUint(lastRoutePoolId, 10) != tokenOutPoolId {
+		return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
+	}
+
+	sf.TokenOut = tokenOut
+
+	return err
+}
+
+// This code is currently untested since I cannot find a TX execution for this
+// It should be fine for the time being since it is following the same pattern established for GAMM SwapExactAmountOut, which the poolmanager will call
+func (sf *WrapperMsgSwapExactAmountOut) HandleMsg(msgType string, msg sdk.Msg, log *txModule.LogMessage) error {
+	sf.Type = msgType
+	sf.OsmosisMsgSwapExactAmountOut = msg.(*poolManagerTypes.MsgSwapExactAmountOut)
+
+	// The attribute in the log message that shows you the tokens swapped
+	tokensSwappedEvt := txModule.GetEventWithType("token_swapped", log)
+	if tokensSwappedEvt == nil {
+		return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
+	}
+
+	// This gets the first token swapped in (if there are multiple pools we do not care about intermediates)
+	tokenInStr := txModule.GetValueForAttribute("tokens_in", tokensSwappedEvt)
+	tokenIn, err := sdk.ParseCoinNormalized(tokenInStr)
+	if err != nil {
+		return &txModule.MessageLogFormatError{MessageType: msgType, Log: fmt.Sprintf("%+v", log)}
+	}
+	sf.TokenIn = tokenIn
+
+	sf.Address = sf.OsmosisMsgSwapExactAmountOut.Sender
+	sf.TokenOut = sf.OsmosisMsgSwapExactAmountOut.TokenOut
+	return err
+}
+
+func (sf *WrapperMsgSwapExactAmountIn) ParseRelevantData() []parsingTypes.MessageRelevantInformation {
+	var relevantData = make([]parsingTypes.MessageRelevantInformation, 1)
+	relevantData[0] = parsingTypes.MessageRelevantInformation{
+		AmountSent:           sf.TokenIn.Amount.BigInt(),
+		DenominationSent:     sf.TokenIn.Denom,
+		AmountReceived:       sf.TokenOut.Amount.BigInt(),
+		DenominationReceived: sf.TokenOut.Denom,
+		SenderAddress:        sf.Address,
+		ReceiverAddress:      sf.Address,
+	}
+	return relevantData
+}
+
+func (sf *WrapperMsgSwapExactAmountOut) ParseRelevantData() []parsingTypes.MessageRelevantInformation {
+	var relevantData = make([]parsingTypes.MessageRelevantInformation, 1)
+	relevantData[0] = parsingTypes.MessageRelevantInformation{
+		AmountSent:           sf.TokenIn.Amount.BigInt(),
+		DenominationSent:     sf.TokenIn.Denom,
+		AmountReceived:       sf.TokenOut.Amount.BigInt(),
+		DenominationReceived: sf.TokenOut.Denom,
+		SenderAddress:        sf.Address,
+		ReceiverAddress:      sf.Address,
+	}
+	return relevantData
+}
