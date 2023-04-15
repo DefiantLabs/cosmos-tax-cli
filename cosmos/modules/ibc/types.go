60d01f80ede28b5e67c4b26e21d6c1efcb0b2f38
jtieri <justin@thetieris.com>
2023-04-15 14:04:07 -0500

    WIP: adding support for IBC messages

--- cosmos/modules/ibc/types.go
+++ cosmos/modules/ibc/types.go
@@ -7,22 +7,24 @@ import (
 	txModule "github.com/DefiantLabs/cosmos-tax-cli/cosmos/modules/tx"
 	"github.com/DefiantLabs/cosmos-tax-cli/util"
 	"github.com/cosmos/ibc-go/v4/modules/apps/transfer/types"
+	chantypes "github.com/cosmos/ibc-go/v4/modules/core/04-channel/types"
 
 	stdTypes "github.com/cosmos/cosmos-sdk/types"
 )
 
 const (
-	MsgTransfer = "/ibc.applications.transfer.v1.MsgTransfer"
+	MsgTransfer        = "/ibc.applications.transfer.v1.MsgTransfer"
+	MsgAcknowledgement = "/ibc.core.channel.v1.MsgAcknowledgement"
+	MsgRecvPacket      = "/ibc.core.channel.v1.MsgRecvPacket"
+	MsgTimeout         = "/ibc.core.channel.v1.MsgTimeout"
 
 	// Explicitly ignored messages for tx parsing purposes
-	MsgAcknowledgement    = "/ibc.core.channel.v1.MsgAcknowledgement"
 	MsgChannelOpenTry     = "/ibc.core.channel.v1.MsgChannelOpenTry"
 	MsgChannelOpenConfirm = "/ibc.core.channel.v1.MsgChannelOpenConfirm"
 	MsgChannelOpenInit    = "/ibc.core.channel.v1.MsgChannelOpenInit"
 	MsgChannelOpenAck     = "/ibc.core.channel.v1.MsgChannelOpenAck"
-	MsgRecvPacket         = "/ibc.core.channel.v1.MsgRecvPacket"
-	MsgTimeout            = "/ibc.core.channel.v1.MsgTimeout"
-	MsgTimeoutOnClose     = "/ibc.core.channel.v1.MsgTimeoutOnClose"
+
+	MsgTimeoutOnClose = "/ibc.core.channel.v1.MsgTimeoutOnClose"
 
 	MsgConnectionOpenTry     = "/ibc.core.connection.v1.MsgConnectionOpenTry"
 	MsgConnectionOpenConfirm = "/ibc.core.connection.v1.MsgConnectionOpenConfirm"
@@ -79,4 +81,51 @@ func (sf *WrapperMsgTransfer) String() string {
 		return fmt.Sprintf("MsgTransfer: IBC transfer from %s to %s did not include an amount\n", sf.SenderAddress, sf.ReceiverAddress)
 	}
 	return fmt.Sprintf("MsgTransfer: IBC transfer of %s from %s to %s\n", sf.CosmosMsgTransfer.Token, sf.SenderAddress, sf.ReceiverAddress)
+}
+
+type WrapperMsgRecvPacket struct {
+	txModule.Message
+	MsgRecvPacket   *chantypes.MsgRecvPacket
+	SenderAddress   string
+	ReceiverAddress string
+	Amount          stdTypes.Coin
+}
+
+func (w *WrapperMsgRecvPacket) HandleMsg(msgType string, msg stdTypes.Msg, log *txModule.LogMessage) error {
+	w.Type = msgType
+	w.MsgRecvPacket = msg.(*chantypes.MsgRecvPacket)
+
+	// Confirm that the action listed in the message log matches the Message type
+	validLog := txModule.IsMessageActionEquals(w.GetType(), log)
+	if !validLog {
+		return util.ReturnInvalidLog(msgType, log)
+	}
+
+	// Unmarshal the json encoded packet data so we can access sender, receiver and denom info
+	var data types.FungibleTokenPacketData
+	if err := types.ModuleCdc.UnmarshalJSON(w.MsgRecvPacket.Packet.GetData(), &data); err != nil {
+		return err
+	}
+
+	w.SenderAddress = data.Sender
+	w.ReceiverAddress = data.Receiver
+
+	amount, ok := stdTypes.NewIntFromString(data.Amount)
+	if !ok {
+		return fmt.Errorf("failed to convert denom amount to sdk.Int, got(%s)", data.Amount)
+	}
+
+	w.Amount = stdTypes.NewCoin(data.Denom, amount)
+
+	return nil
+}
+
+func (w *WrapperMsgRecvPacket) ParseRelevantData() []parsingTypes.MessageRelevantInformation {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (w *WrapperMsgRecvPacket) String() string {
+	//TODO implement me
+	panic("implement me")
 }
