c56c4d1036d096001459ea612acbfd83b6d81d10
pharr117 <pjharral@gmail.com>
2024-04-27 14:33:10 -0400

    Add -ff flag to enable flatten and fill since this is the bottleneck

--- tools/find-block-height-gaps/main.py
+++ tools/find-block-height-gaps/main.py
@@ -18,11 +18,12 @@ def get_env():
 
     return ret
 
-def get_chain_id_arg():
+def get_args():
     parser = argparse.ArgumentParser(description="Dump failed block heights")
     parser.add_argument("--chain-id", type=str, default="osmosis-1", help="Chain ID to dump failed block heights from")
+    parser.add_argument("--flatten-and-fill", "-ff", action="store_true", help="Flatten and fill the gaps and output to a file")
     args = parser.parse_args()
-    return args.chain_id
+    return args
 
 SELECT_CHAINS_QUERY = "SELECT id FROM chains WHERE chain_id=%s;"
 GAPS_QUERY = """
@@ -36,9 +37,18 @@ FROM (
 WHERE height + 1 <> next_height;
 """
 
+def flatten_and_fill(gaps):
+    flat = []
+    for gap in gaps:
+        flattened_and_filled = [x for x in range(gap[0], gap[1] + 1)]
+        flat.extend(flattened_and_filled)
+    return flat
+
 if __name__ == "__main__":
     env = get_env()
-    chain_id = get_chain_id_arg()
+    args = get_args()
+    chain_id = args.chain_id
+    ff = args.flatten_and_fill
     os.makedirs("./output", exist_ok=True)
 
     conn = psycopg2.connect(f"dbname={env['db_name']} user={env['user']} host={env['host']} password={env['password']} port={env['port']}")
@@ -46,24 +56,27 @@ if __name__ == "__main__":
     try:
 
         rec = None
+        print(f"Finding chain ID {chain_id}...")
         with conn.cursor() as cur:
             cur.execute(SELECT_CHAINS_QUERY, (chain_id,))
             rec = cur.fetchone()
             if rec is None:
                 raise Exception(f"Chain ID {chain_id} not found")
-
+        
+        print("Executing gap finder...")
         with conn.cursor() as cur:
             cur.execute(GAPS_QUERY, (rec[0],))
             gaps = cur.fetchall()
 
-        # flatten and fill the groups to get a list of missing heights
-        flat = []
-        for gap in gaps:
-            flattened_and_filled = [x for x in range(gap[0], gap[1] + 1)]
-            flat.extend(flattened_and_filled)
 
         json.dump(gaps, open("output/gaps.json", 'w'), indent=4)
-        json.dump(flat, open("output/missing_heights.json", 'w'), indent=4)
+
+        if ff and len(gaps) > 0:
+            print("Found gaps, flattening and filling...")
+            flat = flatten_and_fill(gaps)
+            json.dump(flat, open("output/missing_heights.json", 'w'), indent=4)
+
+        print("Done")
     except Exception as err:
         print(err)
         traceback.print_exc()
